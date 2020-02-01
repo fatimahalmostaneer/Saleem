08198a6164c425e76f15379b2f86542252278d58
fatimahalmostaneer <55321790+fatimahalmostaneer@users.noreply.github.com>
2020-02-01 17:29:04 +0300

    Testing push

--- app/src/main/java/sa/ksu/gpa/saleem/MainActivity.kt
+++ app/src/main/java/sa/ksu/gpa/saleem/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //tomuch
         //setSupportActionBar(toolbar)
 
        // fab.setOnClickListener { view ->
