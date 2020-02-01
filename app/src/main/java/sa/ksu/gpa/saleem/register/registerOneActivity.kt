7674964430e288996a5a0df92cc4f663d09e1be1
ghedaa <ghedaa.aj@gmail.com>
2020-02-01 23:40:15 +0300

    Merge branch 'master' of C:\Users\gheda\Documents\GitHub\Saleem with conflicts.

---
+++ app/src/main/java/sa/ksu/gpa/saleem/register/registerOneActivity.kt
@@ -0,0 +1,14 @@
+package sa.ksu.gpa.saleem.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sa.ksu.gpa.saleem.R
+
+class registerOneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_one)
+
+    }
+}
