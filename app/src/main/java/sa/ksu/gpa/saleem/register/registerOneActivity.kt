0b5f41c2fd54c4a4117b1e861bf3dcf6022dd865
ghedaa <ghedaa.aj@gmail.com>
2020-02-02 23:57:34 +0300

    sign up layout

--- app/src/main/java/sa/ksu/gpa/saleem/register/registerOneActivity.kt
+++ app/src/main/java/sa/ksu/gpa/saleem/register/registerOneActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
@@ -13,13 +14,28 @@ import sa.ksu.gpa.saleem.R
 
 class registerOneActivity : AppCompatActivity() {
 
-    val emailTxt = findViewById<View>(R.id.emailET) as EditText
-    val nameTxt = findViewById<View>(R.id.nameET) as EditText
+   /* val emailTxt = findViewById<View>(R.id.emailET) as EditText
+    val nameTxt = findViewById<View>(R.id.nameET) as EditText*/
+    val btn=findViewById<View>(R.id.nxtOneBtn) as Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_one)
 
+       // emailTxt.callOnClick()
+        btn?.setOnClickListener {
+            Toast.makeText(this@registerOneActivity, "Click...", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, registerTwoActivity::class.java)
+// To pass any data to next activity
+            //intent.putExtra("keyIdentifier", value)
+// start your next activity
+            startActivity(intent)
+        }
+
+
+
+/*
         val db = FirebaseFirestore.getInstance()
         val TAG = "MyActivity"
 
@@ -38,7 +54,7 @@ class registerOneActivity : AppCompatActivity() {
             }
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
-            }
+            }*/
 
     /*    if (!email.isEmpty() && !password.isEmpty() && !name.isEmpty()) {
 
