9565a24b5b255f6cedd188129ba4e0dbe06e0a19
ghedaa <ghedaa.aj@gmail.com>
2020-02-04 21:51:59 +0300

    linked page1

--- app/src/main/java/sa/ksu/gpa/saleem/register/registerTwoActivity.kt
+++ app/src/main/java/sa/ksu/gpa/saleem/register/registerTwoActivity.kt
@@ -1,15 +1,17 @@
 package sa.ksu.gpa.saleem.register
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_register_two.*
 import sa.ksu.gpa.saleem.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class registerTwoActivity : AppCompatActivity() {
 /*
@@ -19,6 +21,9 @@ class registerTwoActivity : AppCompatActivity() {
     val db = FirebaseFirestore.getInstance()
     val TAG = "MyActivity"*/
 
+    var button_date: Button? = null
+    var textview_date: TextView? = null
+    var cal = Calendar.getInstance()
 
 
 
@@ -54,6 +59,42 @@ class registerTwoActivity : AppCompatActivity() {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error adding document", e)
             }*/
+        // get the references from layout file
+        textview_date = this.text_view_date_1
+        button_date = this.button_date_1
 
+        textview_date!!.text = "--/--/----"
+
+        // create an OnDateSetListener
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
+        button_date!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@registerTwoActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+
+        })
     }
+
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        textview_date!!.text = sdf.format(cal.getTime())
+    }
+
 }
+
