a73da1aaa341d604c522a879ea9a491a257c3f89
fatimahalmostaneer <55321790+fatimahalmostaneer@users.noreply.github.com>
2020-02-04 23:37:06 +0300

    Type: Adding Scanner system changes : adding scanner class and changes in the main two wxtra methods and a button

--- app/src/main/java/sa/ksu/gpa/saleem/ScanActivity.kt
+++ app/src/main/java/sa/ksu/gpa/saleem/ScanActivity.kt
@@ -1,46 +1,83 @@
 package sa.ksu.gpa.saleem
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.zxing.Result
+import kotlinx.android.synthetic.main.activity_scan.*
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 class ScanActivity : AppCompatActivity() ,ZXingScannerView.ResultHandler{
-    private var mScannerView: ZXingScannerView? = null
-    val db = FirebaseFirestore.getInstance()
-
+    private lateinit var mScannerView: ZXingScannerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
-        setContentView(mScannerView)
-    }
-    public override fun onResume() {
-        super.onResume()
-        mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
-        mScannerView!!.startCamera()          // Start camera on resume
-    }
+        setContentView(R.layout.activity_scan)
 
-    public override fun onPause() {
-        super.onPause()
-        mScannerView!!.stopCamera()           // Stop camera on pause
+        initScannerView()
+
+
+
     }
 
-    override fun handleResult(rawResult: Result) {
-
-         db.collection("Products").document(rawResult.text).get()
-            .addOnSuccessListener { document ->
-                if (document !=null){
-                    Log.d("tag", "sucees ")
-
-                }
-            }.addOnFailureListener{exception ->
-                Log.d("tag", "get failed with ", exception)
+    private fun initScannerView() {
+        mScannerView = ZXingScannerView(this)
+        mScannerView.setAutoFocus(true)
+        mScannerView.setResultHandler(this)
+        frame_layout_camera.addView(mScannerView)
+    }
+    override fun onStart() {
+        mScannerView.startCamera()
+        doRequestPermission()
+        super.onStart()
+    }
+    private fun doRequestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
             }
+        }
+    }
 
-        onBackPressed()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            100 -> {
+                initScannerView()
+            }
+            else -> {
+            }
+        }
+    }
+
+    override fun onPause() {
+        mScannerView.stopCamera()
+        super.onPause()
+    }
+    override fun handleResult(p0: Result?) {
+        productNotFoundedDialog()
+    }
+
+    private fun productNotFoundedDialog() {
+       SweetAlertDialog(this,SweetAlertDialog.WARNING_TYPE)
+            .setTitleText("نعتذر لك")
+            .setContentText("هذا المنتج غير متوفر")
+            .setConfirmButton("حسنًا") { sDialog -> sDialog.dismissWithAnimation()
+               finish()
+
+           }
+            .show()
 
     }
+
+
 }
