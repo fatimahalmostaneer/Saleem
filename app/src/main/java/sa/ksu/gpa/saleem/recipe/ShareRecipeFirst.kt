b6f791250714bdbac1d113f4480c8c3828a91857
RahafAlq <57556748+RahafAlq@users.noreply.github.com>
2020-02-11 22:08:43 +0300

    test bush and the last add

---
+++ app/src/main/java/sa/ksu/gpa/saleem/recipe/ShareRecipeFirst.kt
@@ -0,0 +1,13 @@
+package sa.ksu.gpa.saleem.recipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sa.ksu.gpa.saleem.R
+
+class ShareRecipeFirst : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_recipe_first)
+    }
+}
